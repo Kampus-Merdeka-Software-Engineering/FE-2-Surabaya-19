74f7843c93869072f70feef419b1b08e9eb94b25
Andra Yusaldi Putra <andrayusaldi9@gmail.com>
2023-12-06 23:21:03 +0700

    update

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const API_BASE_URL =
+  "https://mysql://root:16cB52dGFcG3DH6B1Dcfb122-3e5FCga@monorail.proxy.rlwy.net:40172/railway";
+
+async function fetchBooks() {
+  const bookList = document.getElementById("books-list");
+  try {
+    const response = await fetch(`${API_BASE_URL}/books`);
+    const books = await response.json();
+    const bookListElement = books.map((book) => {
+      return `
+        <li>${book.title} by ${book.author}</li>
+      `;
+    });
+
+    bookList.innerHTML = bookListElement;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+fetchBooks();
+
+async function addBook() {
+  const title = document.getElementById("book-title").value;
+  const author = document.getElementById("book-author").value;
+
+  try {
+    await fetch(`${API_BASE_URL}/books`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title, author }),
+    });
+  } catch (err) {
+    console.error(err);
+  } finally {
+    fetchBooks();
+  }
+}
