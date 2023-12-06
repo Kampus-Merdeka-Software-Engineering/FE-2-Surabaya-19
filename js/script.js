429b8fe53dfc2452a4e765d5074d3c0207284ea4
Andra Yusaldi Putra <andrayusaldi9@gmail.com>
2023-12-07 02:23:03 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-const API_BASE_URL = "https:/be-2-surabaya-19-production.up.railway.app";
+document.querySelector(".menu-icon").addEventListener("click", function () {
+  this.classList.toggle("change");
+  doument.querySelector(".top-navbar ul").classList.toggle("show");
+});
+
+const API_BASE_URL = "http:/be-2-surabaya-19-production.up.railway.app";
 
 async function fetchMenus() {
   const menuContainer = document.getElementById("menu-container");
@@ -7,7 +12,7 @@ async function fetchMenus() {
     const menus = await response.json();
     console.log(menu);
 
-    const menuListElement = menus.map((item) => {
+    const menu = menus.map((item) => {
       return `
       <div class="menu-section">
       <img src="${item.image}" alt="" class="menu-img" />
