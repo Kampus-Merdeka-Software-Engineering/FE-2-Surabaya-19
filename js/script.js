a3b76427dba1620c9def72916e31fd7182f6a923
Andra Yusaldi Putra <andrayusaldi9@gmail.com>
2023-12-07 03:07:06 +0700

    test

--- js/script.js
+++ js/script.js
@@ -12,14 +12,14 @@ async function fetchMenus() {
     const menus = await response.json();
     console.log(menus); // Fix the typo here
 
-    const menuListElement = menus.map((item) => {
+    const menuListElement = menus.map((menus) => {
       return `
         <div class="menu-section">
-          <img src="${item.image}" alt="" class="menu-img" />
+          <img src="${menus.image}" alt="" class="menu-img" />
           <div class="text-section">
-            <h4>${item.name}</h4>
-            <p>${item.desc}</p>
-            <h4>${item.price}</h4>
+            <h4>${menus.name}</h4>
+            <p>${menus.desc}</p>
+            <h4>${menus.price}</h4>
           </div>
           <button id="psn">Tambahkan</button>
         </div>
