9f1146dd32e231c880dcfb45af8171b6f9d3305a
Andra Yusaldi Putra <andrayusaldi9@gmail.com>
2023-12-07 18:15:06 +0700

    update

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ document.querySelector(".menu-icon").addEventListener("click", function () {
   document.querySelector(".top-navbar ul").classList.toggle("show");
 });
 
-const API_BASE_URL = "https:/be-2-surabaya-19-production.up.railway.app";
+const API_BASE_URL = "https://be-2-surabaya-19-production.up.railway.app";
 
 async function fetchMenus() {
-  const menuContainer = document.getElementById("menu-container");
+  const menuContainer = document.getElementById("menu-cont");
   try {
     const response = await fetch(`${API_BASE_URL}/menus`);
     const menus = await response.json();
@@ -15,11 +15,11 @@ async function fetchMenus() {
     const menuContainerElement = menus.map((menu) => {
       return `
         <div class="menu-section">
-          <img src="${menu.image}" alt="" class="menu-img" />
+          <img src="${menu.image}" alt="" class="menu-img" id="menu-image"/>
           <div class="text-section">
-            <h4>${menu.name}</h4>
-            <p>${menu.desc}</p>
-            <h4>${menu.price}</h4>
+            <h4 id="menu-name">${menu.name}</h4>
+            <p id="menu-desc">${menu.desc}</p>
+            <h4 id="menu-price">${menu.price}</h4>
           </div>
           <button id="psn">Tambahkan</button>
         </div>
@@ -32,3 +32,24 @@ async function fetchMenus() {
   }
 }
 fetchMenus();
+
+async function addMenu() {
+  const name = document.getElementById("menu-name").value;
+  const desc = document.getElementById("menu-desc").value;
+  const price = document.getElementById("menu-price").value;
+  const image = document.getElementById("menu-image").value;
+
+  try {
+    await fetch(`${API_BASE_URL}/menus`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, desc, price, image }),
+    });
+  } catch (err) {
+    console.error(err);
+  } finally {
+    fetchBooks();
+  }
+}
