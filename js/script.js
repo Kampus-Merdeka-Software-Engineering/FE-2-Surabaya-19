a36ad32695039aeb23103ab5f89e7fe63eb437d7
Andra Yusaldi Putra <andrayusaldi9@gmail.com>
2023-12-11 00:48:07 +0700

    perbaikan bug

--- js/script.js
+++ js/script.js
@@ -1,11 +1,34 @@
 const API_BASE_URL = "https://be-2-surabaya-19-production.up.railway.app";
 
+async function addMenu(name, desc, price, image) {
+  try {
+    const response = await fetch(`${API_BASE_URL}/menus`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, desc, price, image }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to add menu: ${response.statusText}`);
+    }
+  } catch (err) {
+    console.error(err);
+  } finally {
+    fetchMenus();
+  }
+}
+
 async function fetchMenus() {
   const menuContainer = document.getElementById("menu-cont");
   try {
     const response = await fetch(`${API_BASE_URL}/menus`);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch menus: ${response.statusText}`);
+    }
+
     const menus = await response.json();
-    console.log(menus); // Fix the typo here
 
     const menuContainerElement = menus.map((menu) => {
       return `
@@ -16,29 +39,15 @@ async function fetchMenus() {
             <p>${menu.desc}</p>
             <h4>Rp. ${menu.price}</h4>
           </div>
-          <button id="psn">Tambahkan</button>
+          <button class="psn-btn">Tambahkan</button>
         </div>`;
     });
 
-    menuContainer.innerHTML = menuContainerElement;
+    menuContainer.innerHTML = menuContainerElement.join("");
   } catch (err) {
     console.error(err);
   }
 }
-fetchMenus();
 
-async function addMenu(name, desc, price, image) {
-  try {
-    await fetch(`${API_BASE_URL}/menus`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name, desc, price, image }),
-    });
-  } catch (err) {
-    console.error(err);
-  } finally {
-    fetchMenus();
-  }
-}
+// Example usage:
+// addMenu("New Item", "Description", 10.99, "image-url");
