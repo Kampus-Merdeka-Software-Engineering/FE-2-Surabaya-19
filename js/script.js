79400e702483eade01ebd9fea8905dc469f59fc8
Andra Yusaldi Putra <andrayusaldi9@gmail.com>
2023-12-11 15:48:45 +0700

    bug fix

--- js/script.js
+++ js/script.js
@@ -6,64 +6,36 @@ async function addMenus() {
   const price = document.getElementById("price").value;
   const desc = document.getElementById("desc").value;
   const image = document.getElementById("image").value;
-
   try {
     const response = await fetch(`${API_BASE_URL}/menus`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "POST,PATCH,OPTIONS",
       },
       body: JSON.stringify({ name, desc, price, image }),
     });
-
     if (!response.ok) {
       throw new Error("Failed to add item to cart");
     }
-
-    const result = await response.json(); // Parse response JSON
-    console.log("Item added to cart:", result); // Access response data
-
+    console.log("Item added to cart:", response.json());
     // Update UI or perform other actions after adding to cart
   } catch (error) {
     console.error("Error adding item to cart:", error);
   }
 }
 
-// Function to delete item
-async function deleteMenu(menuId) {
-  try {
-    const response = await fetch(`${API_BASE_URL}/menus/${menuId}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (!response.ok) {
-      throw new Error("Failed to delete item");
-    }
-
-    const result = await response.json(); // Parse response JSON
-    console.log("Item deleted:", result); // Access response data
-
-    // Update UI or perform other actions after deletion
-  } catch (error) {
-    console.error("Error deleting item:", error);
-  }
-}
-
 async function renderMenus() {
-  const menuSection = document.getElementById("menu-cont");
-
+  const menusection = document.getElementById("menu-cont"); // Update to menu-container
   try {
-    const response = await fetch(`${API_BASE_URL}/menus`);
-
+    const response = await fetch(`${API_BASE_URL}/menus`); // Assuming /api/food is the correct endpoint
     if (!response.ok) {
       throw new Error("Failed to fetch data");
     }
-
     const menus = await response.json();
 
+    // Generating HTML content using map
     const htmlContent = menus
       .map(
         (menu) => `
@@ -74,13 +46,13 @@ async function renderMenus() {
                   <p>${menu.desc}</p>
                   <h4>Rp.${menu.price}</h4>
               </div>
-              <button onclick="deleteMenu(${menu.id})" type="button">delete</button>
+              
           </div>
       `
       )
       .join("");
 
-    menuSection.innerHTML = htmlContent;
+    menusection.innerHTML = htmlContent; // Set the generated HTML content to menu-container
   } catch (error) {
     console.error("Error fetching data:", error);
   }
